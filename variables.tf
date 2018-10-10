a07c826a3274d99180e9714549b4656f6997f2c0
Sébastien Braun <sebbraun@gmail.com>
2018-10-10 16:52:46 +0200

    patched variable.tf

--- variables.tf
+++ variables.tf
@@ -1,5 +1,5 @@
 variable region {
-  description "The region to operate under"
+  description = "The region to operate under"
   default = "europe-west1"
 }
 
@@ -8,30 +8,30 @@ variable zone {
   default = "europe-west1-c"
 }
 
-variable "project_name" {
+variable project_name {
   description = "GCP project targeted"
 }
 
-variable "target_tags" {
+variable target_tags {
   description = "List of target tags for health check firewall rule."
   default = "vault"
 }
 
-variable "private_key_pem" {
+variable private_key_pem {
   description = "Load Balancer TLS Certificate Private key"
 }
 
-variable "cert_pem" {
+variable cert_pem {
   description = "Load Balancer TLS Certificate"
 }
 
-variable "vault_instance_names" {
+variable vault_instance_names {
   description = "list of Vault Server to Load Balance to"
   type = "list"
   default = ["vault-01", "vault-02"]
 }
 
-variable "http_forward" {
-  description = "by default we disable HTTP Forwarding"
+variable http_forward {
+  description = "Set to `false` to disable HTTP port 80 forward"
   default = "false"
 }

b1b1f206e82f575ada4cfe79fa87a6cca285b45a
Sébastien Braun <sebbraun@gmail.com>
2018-10-15 14:55:46 +0200

    output patched

--- main.tf
+++ main.tf
@@ -46,5 +46,5 @@ resource "google_compute_instance_group" "vaults" {
 
 output public_ip {
   description = "The external IP assigned to the Global forwarding rule."
-  value = ${module.gce-lb-https.external_ip}
+  value = "${module.gce-lb-https.external_ip}"
 }

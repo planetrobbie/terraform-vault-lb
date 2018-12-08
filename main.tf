11d061955fe72ed873e78071487f0cd1d023b13d
Sébastien Braun <sebbraun@gmail.com>
2018-12-08 11:20:10 +0100

    outputs moved away from main.tf

--- main.tf
+++ main.tf
@@ -43,8 +43,3 @@ resource "google_compute_instance_group" "vaults" {
 
   zone = "${var.zone}"
 }
-
-output public_ip {
-  description = "The external IP assigned to the Global forwarding rule."
-  value = "${module.gce-lb-https.external_ip}"
-}
--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,4 @@
-output "GSLB external-ip" {
-  value = ["${module.gce-lb-https.external_ip}"]
+output public_ip {
+  description = "The external IP assigned to the Global forwarding rule."
+  value = "${module.gce-lb-https.external_ip}"
 }

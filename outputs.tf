c6778dcee3f3334453e7b6424122755a94d07042
Sébastien Braun <sebbraun@gmail.com>
2018-12-08 11:53:10 +0100

    FQDN added to output

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,8 @@ output public_ip {
   description = "The external IP assigned to the Global forwarding rule."
   value = "${module.gce-lb-https.external_ip}"
 }
+
+output fqdn {
+  description = "Fully Qualified Domain Name of your Google Cloud GSLB"
+  valut = "vault.${var.gcp_dns_domain}"
+}
